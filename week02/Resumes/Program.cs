63eb00de264de5533f9607bd2632e024f4d4dc48
Ms-Schnitzel <zachnielsen72@gmail.com>
2026-01-12 20:45:14 -0700

    week2 resume project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2001;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._company = "Amazon";
+        job2._jobTitle = "Marketing Lead";
+        job2._startYear = 2010;
+        job2._endYear = 2025;
+
+        // job1.Display();
+        // job2.Display();
+
+        Resume resumes = new Resume();
+        resumes._name = "Alice Wonderland";
+        resumes._jobs.Add(job1);
+        resumes._jobs.Add(job2);
+
+        // resumes._jobs[0].Display();
+        resumes.Display();
+
+
+
     }
 }
