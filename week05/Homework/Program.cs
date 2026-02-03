ac6aa0ab4471c5d94ce881b2891c899823ecee48
Ms-Schnitzel <zachnielsen72@gmail.com>
2026-02-02 22:34:42 -0700

    week5 Homework Practice

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment testAssign = new Assignment("Johnny", "Science");
+        Console.WriteLine(testAssign.GetSummary());
+        MathAssignment testMath = new MathAssignment("Jill", "Math", "Section 17", "23-45");
+        Console.WriteLine(testMath.GetHomeworkList());
+        WritingAssignment testWrite = new WritingAssignment("Amy", "English", "History of WW1");
+        Console.WriteLine(testWrite.GetWritingInformation());
     }
 }
