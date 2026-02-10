f27fdd9b09b748cff954467318fbe32cb6a682e3
Ms-Schnitzel <zachnielsen72@gmail.com>
2026-02-09 22:33:00 -0700

    week 6 shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square testSq = new Square("red", 5);
+        Rectangle testRec = new Rectangle("blue", 7, 4);
+        Circle testCir = new Circle("white", 3);
+        
+
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(testSq);
+        shapeList.Add(testRec);
+        shapeList.Add(testCir);
+
+        foreach(Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
